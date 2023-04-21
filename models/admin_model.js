d91ce68d985085788e116ff674c433d934cc581a
andavarapulikith <likithandavarapuiiits@gmail.com>
2023-04-21 14:59:25 +0530

    changes in admin panel

---
+++ models/admin_model.js
@@ -0,0 +1,61 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const admin_schema=new Schema({
+    password: {
+        type: String,
+        required:[true,'Password is required']
+    },
+    firstname:{
+        type: String,
+        required: [true, 'firstname is required'],
+        
+    },
+    lastname: {
+        type: String,
+        required: [true, 'lastname is required']
+    },
+    domain: {
+        type: String,
+        default: 'not specified'
+    },
+    email: {
+        type: String,
+        required: [true, 'gmail is required']
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone number is required']
+    },
+    gender: {
+        type: String,
+        default:'not specified'
+    },
+    insta_link: {
+        type: String,
+        default:'/'
+    },
+    facebook_link: {
+        type: String,
+        default:'/'
+    },
+    github_link: {
+        type: String,
+        default:'/'
+    },
+    qualification:{
+         type:String,
+         default:'not specified'
+    },
+    dateofbirth:{
+        type:Date,
+        default:Date.now()
+    },
+    profile_image_link: {
+        type: String,
+        default:'https://th.bing.com/th/id/OIP.z4no5tqp2ryBdMMD5NU9OgHaEv?w=245&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    }
+})
+const admin_model = mongoose.model('admin_model', admin_schema);
+
+module.exports = admin_model;
--- routes/expert_articles_routes.js
+++ routes/expert_articles_routes.js
@@ -4,14 +4,21 @@ const article_model = require('../models/article_model');
 const {isAuth} = require('../controllers/isAuth');
 
 router.get('/',isAuth, (req, res) => {
+
     const registeras = req.session.registeras;
+    if(registeras=='expert'||registeras=='admin')
+    {
     const expert_id = req.session.profile_data;
     article_model.find({ author_id: expert_id }).then((expert_articles_data) => {
 
             res.render('expert_articles', { data: expert_articles_data, 'registeras': registeras });
 
     })
-
+}
+else
+{
+    res.render('notfound')
+}
 });
 
 router.get('/delete/:article_id',isAuth, (req, res) => {
